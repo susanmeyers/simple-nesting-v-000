d70b60c8c37ff318fbc11744b92ab90a546449cb
Susan Meyers <smmeyers24@gmail.com>
2018-08-28 00:52:14 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -110,7 +110,7 @@ def changing_alan
      #alans_new_info = "GUI"
 
   programmer_hash[:alan_kay][:known_for] = "GUI"
-    return programmer_hash[:known_for]
+    return programmer_hash[alan_kay][:known_for]
 end
 
 def adding_to_dennis
